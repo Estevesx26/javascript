0ba4ad1a95f7139defb2bacd1719d597d3bf70a4
Estevesx26 <emmanuelestevesdeoliveira@gmail.com>
2026-05-06 09:39:19 -0300

    ex condicao

---
+++ cursojs/aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 80
+var svel = 70
+console.log(`A velocidade máxima da avenida é ${vel}km/h`)
+if (svel > 80){
+    console.log(`Você ultrapassou a velocidade de ${vel}km/h. MULTADO.`)
+} else {
+    console.log(`Continue dirigindo na velocidade permitida de ${vel}km/h`)
+}
+console.log(`Dirija sempre com atenção, não ultrapasse o limite de ${vel}km/h!`)
