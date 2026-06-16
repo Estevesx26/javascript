dbef57982b18bb6899178daf7d8adebf31845cd9
Estevesx26 <emmanuelestevesdeoliveira@gmail.com>
2026-06-16 07:47:28 -0300

    up

---
+++ cursojs/aula12ex/aula13/ambiente.js
@@ -0,0 +1,16 @@
+var c = 1
+while ( c < 20){
+    console.log('Tudo bem')
+    c++
+}
+
+
+
+
+
+/*.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')
+console.log('Tudo bem')*/
