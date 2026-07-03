0a97742a6f54a3cb0408ed68aa59c17a0d72348d
Estevesx26 <emmanuelestevesdeoliveira@gmail.com>
2026-07-03 14:14:30 -0300

    up

---
+++ cursojs/aula13/ambiente.js
@@ -0,0 +1,26 @@
+var c = 1
+do{
+    console.log(`passo ${c}`)
+    c++
+} while (c <= 8)
+
+/* 
+var c = 1
+
+while(c <=8){
+    console.log(`passo ${c}`)
+    c++ // c = c + 1
+}
+/*
+
+/* 
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+console.log('tudo bem')
+*/
