96bf99627cd2eed525a55774ec5611b70c82e6f3
Estevesx26 <emmanuelestevesdeoliveira@gmail.com>
2026-07-08 17:29:48 -0300

    up

---
+++ cursojs/aula14ex/ex016/modelo.js
@@ -0,0 +1,9 @@
+function clicar(){
+    var inicial = Number.parseFloat(document.getElementById('inicio').value)
+    var fim = Number.parseFloat(document.getElementById('fim').value)
+    var passo = Number.parseFloat(document.getElementById('passo').value)
+    var btn = document.getElementById('btn')
+    var txt = document.getElementById('txt')
+
+
+}
